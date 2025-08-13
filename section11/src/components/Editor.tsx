e4f68007dfeedda726dc0aa2a0e6cf5cc760b515
haleychoioi <reum01060106@gmail.com>
2025-08-13 13:59:18 +0900

    8/13 study

---
+++ section11/src/components/Editor.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useTodoDispatch } from "../App";
+
+export default function Editor() {
+  const dispatch = useTodoDispatch();
+
+  const [text, setText] = useState("");
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    dispatch.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <>
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </>
+  );
+}
